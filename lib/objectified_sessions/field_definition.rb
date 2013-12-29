1e614f7517e0dcf8983fe34ee41c19ecd78b7dfc
Andrew Geweke <andrew@geweke.org>
2013-12-28 21:34:02 -0500

    Documentation and specs for FieldDefinition.

--- lib/objectified_sessions/field_definition.rb
+++ lib/objectified_sessions/field_definition.rb
@@ -1,6 +1,11 @@
 module ObjectifiedSessions
+  # A FieldDefinition represents, well, the definition of a single field against a single class (which must be a
+  # descendant of ObjectifiedSessions::Base). It knows how to respond to a few questions, and is responsible for
+  # creating appropriate delegated methods in its owning class's +_dynamic_methods_module+.
   class FieldDefinition
     class << self
+      # Normalizes the name of a field. We use this method to make sure we don't get confused between Strings and
+      # Symbols, and so on.
       def normalize_name(name)
         unless name.kind_of?(String) || name.kind_of?(Symbol)
           raise ArgumentError, "A field name must be a String or Symbol, not: #{name.inspect}"
@@ -12,7 +17,19 @@ module ObjectifiedSessions
 
     attr_reader :name, :storage_name
 
+    # Creates a new instance. +session_class+ must be the Class that you're using as your objectified-session class --
+    # _i.e._, a subclass of ObjectifiedSessions::Base. +name+ is the name of the field. +options+ are the options for
+    # the field:
+    #
+    # [:type] Required; must be one of: +:normal+, +:retired+, or +:inactive+, each corresponding to the field type
+    #         documented in ObjectifiedSessions::Base.
+    # [:storage] If present, this field will use the specified string as the key under which it should be stored; if
+    #            not present, the name will be used instead.
+    # [:visibility] Required; must be +:private+ or +:public+. Methods created on the #_dynamic_methods_module on
+    #               the base class will be of this visibility.
     def initialize(session_class, name, options = { })
+      raise ArgumentError, "Session class must be a Class, not: #{session_class.inspect}" unless session_class.kind_of?(Class)
+
       @session_class = session_class
       @name = self.class.normalize_name(name)
       process_options!(options)
@@ -20,14 +37,20 @@ module ObjectifiedSessions
       create_methods!
     end
 
+    # Returns the key under which this field should read and write its data. This will be its name, unless a
+    # +:storage+ option was passed to the constructor, in which case it will be that value, instead.
     def storage_name
       @storage_name || name
     end
 
+    # If someone has set <tt>unknown_fields :delete</tt> on the base class, should we delete data with this field's
+    # #storage_name anyway? This is true only for retired fields.
     def delete_data_with_storage_name?
       type == :retired
     end
 
+    # Should we allow users to access the data in this field? Retired and inactive fields don't allow access to their
+    # data.
     def allow_access_to_data?
       type == :normal
     end
@@ -35,6 +58,8 @@ module ObjectifiedSessions
     private
     attr_reader :type, :visibility
 
+    # Process the options passed in; this validates them, and sets +@type+, +@visibility+, and +@storage_name+
+    # appropriately.
     def process_options!(options)
       options.assert_valid_keys(:storage, :type, :visibility)
 
@@ -56,6 +81,7 @@ module ObjectifiedSessions
       @visibility = options[:visibility]
     end
 
+    # Creates methods on the dynamic-methods module, as appropriate.
     def create_methods!
       return unless type == :normal
 
---
+++ spec/objectified_sessions/unit/objectified_sessions/field_definition_spec.rb
@@ -0,0 +1,138 @@
+require 'objectified_sessions'
+
+describe ObjectifiedSessions::FieldDefinition do
+  def klass
+    ObjectifiedSessions::FieldDefinition
+  end
+
+  before :each do
+    @session_class = double("session_class")
+    allow(@session_class).to receive(:kind_of?).with(Class).and_return(true)
+
+    @dmm = Module.new do
+      class << self
+        public :define_method, :private
+      end
+    end
+    allow(@session_class).to receive(:_dynamic_methods_module).with().and_return(@dmm)
+  end
+
+  it "should normalize field names properly" do
+    lambda { klass.normalize_name(nil) }.should raise_error(ArgumentError)
+    lambda { klass.normalize_name(12345) }.should raise_error(ArgumentError)
+
+    klass.normalize_name(" fOo  ").should == :fOo
+    klass.normalize_name(:' fOo ').should == :fOo
+    klass.normalize_name(:foo).should == :foo
+    klass.normalize_name("foo").should == :foo
+  end
+
+  it "should validate its constructor arguments appropriately" do
+    lambda { klass.new(double("not_a_class"), :foo, { :type => :normal, :visibility => :private }) }.should raise_error(ArgumentError, /not_a_class/i)
+    lambda { klass.new(@session_class, 12345, { :type => :normal, :visibility => :private }) }.should raise_error(ArgumentError, /12345/i)
+    lambda { klass.new(@session_class, :foo, { :type => :whatever, :visibility => :private }) }.should raise_error(ArgumentError, /whatever/i)
+    lambda { klass.new(@session_class, :foo, { :visibility => :private }) }.should raise_error(ArgumentError, /:type/i)
+    lambda { klass.new(@session_class, :foo, { :type => :normal, :visibility => :whatever }) }.should raise_error(ArgumentError, /whatever/i)
+    lambda { klass.new(@session_class, :foo, { :type => :normal }) }.should raise_error(ArgumentError, /:visibility/i)
+    lambda { klass.new(@session_class, :foo, { :type => :normal, :visibility => :public, :foo => :bar }) }.should raise_error(ArgumentError, /foo/i)
+    lambda { klass.new(@session_class, :foo, { :type => :normal, :visibility => :public, :storage => 12345 }) }.should raise_error(ArgumentError, /12345/i)
+  end
+
+  it "should return the right storage name" do
+    klass.new(@session_class, :foo, { :type => :normal, :visibility => :public }).storage_name.should == 'foo'
+  end
+
+  it "should return the right storage name if aliased" do
+    klass.new(@session_class, :foo, { :type => :normal, :visibility => :public, :storage => :bar }).storage_name.should == 'bar'
+  end
+
+  it "should return the right value for #delete_data_with_storage_name?" do
+    klass.new(@session_class, :foo, { :type => :normal, :visibility => :public }).delete_data_with_storage_name?.should_not be
+    klass.new(@session_class, :foo, { :type => :inactive, :visibility => :public }).delete_data_with_storage_name?.should_not be
+    klass.new(@session_class, :foo, { :type => :retired, :visibility => :public }).delete_data_with_storage_name?.should be
+  end
+
+  it "should return the right value for #allow_access_to_data?" do
+    klass.new(@session_class, :foo, { :type => :normal, :visibility => :public }).allow_access_to_data?.should be
+    klass.new(@session_class, :foo, { :type => :inactive, :visibility => :public }).allow_access_to_data?.should_not be
+    klass.new(@session_class, :foo, { :type => :retired, :visibility => :public }).allow_access_to_data?.should_not be
+  end
+
+  context "with dynamic-methods module testing" do
+    before :each do
+      dmm = @dmm
+      @underlying_session = double("underlying_session")
+
+      @dmm_class = Class.new do
+        include dmm
+
+        def initialize(h)
+          @h = h
+        end
+
+        def [](x)
+          @h[x]
+        end
+
+        def []=(x, y)
+          @h[x] = y
+        end
+      end
+
+      @instance = @dmm_class.new(@underlying_session)
+    end
+
+    it "should create methods on the dynamic-methods module for a normal field" do
+      field = klass.new(@session_class, :foo, { :type => :normal, :visibility => :public })
+
+      expect(@underlying_session).to receive(:[]).once.with(:foo).and_return(:xxx)
+      @instance.foo.should == :xxx
+
+      expect(@underlying_session).to receive(:[]=).once.with(:foo, :zzz).and_return(:yyy)
+      (@instance.foo = :zzz).should == :zzz
+    end
+
+    it "should downcase the field name on the dynamic-methods module for a normal field" do
+      field = klass.new(@session_class, :FoO, { :type => :normal, :visibility => :public })
+
+      expect(@underlying_session).to receive(:[]).once.with(:FoO).and_return(:xxx)
+      @instance.foo.should == :xxx
+
+      expect(@underlying_session).to receive(:[]=).once.with(:FoO, :zzz).and_return(:yyy)
+      (@instance.foo = :zzz).should == :zzz
+    end
+
+    it "should create private methods on the dynamic-methods module for a normal field if marked as private" do
+      field = klass.new(@session_class, :foo, { :type => :normal, :visibility => :private })
+
+      @instance.respond_to?(:foo).should_not be
+      lambda { @instance.foo }.should raise_error(NoMethodError)
+      @instance.respond_to?(:foo=).should_not be
+      lambda { @instance.foo = 123 }.should raise_error(NoMethodError)
+
+      expect(@underlying_session).to receive(:[]).once.with(:foo).and_return(:xxx)
+      @instance.send(:foo).should == :xxx
+
+      expect(@underlying_session).to receive(:[]=).once.with(:foo, :zzz).and_return(:yyy)
+      (@instance.send(:foo=, :zzz)).should == :zzz
+    end
+
+    it "should not create methods on the dynamic-methods module for an inactive field" do
+      field = klass.new(@session_class, :foo, { :type => :inactive, :visibility => :public })
+
+      @instance.respond_to?(:foo).should_not be
+      lambda { @instance.send(:foo) }.should raise_error(NoMethodError)
+      @instance.respond_to?(:foo=).should_not be
+      lambda { @instance.send(:foo=, 123) }.should raise_error(NoMethodError)
+    end
+
+    it "should not create methods on the dynamic-methods module for a retired field" do
+      field = klass.new(@session_class, :foo, { :type => :retired, :visibility => :public })
+
+      @instance.respond_to?(:foo).should_not be
+      lambda { @instance.send(:foo) }.should raise_error(NoMethodError)
+      @instance.respond_to?(:foo=).should_not be
+      lambda { @instance.send(:foo=, 123) }.should raise_error(NoMethodError)
+    end
+  end
+end
